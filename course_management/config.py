50d854a56ae6ba7744bd612e151d1aa4af879c76
Your Name <you@example.com>
2026-02-15 00:43:34 +0530

    removed config.py

--- course_management/config.py
+++
@@ -1,8 +0,0 @@
-class config:
-    SECRET_KEY = "RED"
-    MYSQL_HOST = "localhost"
-    MYSQL_PORT = 3306
-    MYSQL_USER="root"
-    MYSQL_PASSWORD="harsh#122004#"
-    MYSQL_DATABASE="flask_auth"
-    
